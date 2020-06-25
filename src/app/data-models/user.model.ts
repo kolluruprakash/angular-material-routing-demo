5a41501e21fe39ea977bdb44d46884519c20784d
kolluruprakash <kolluruprakash@live.com>
2020-06-25 17:16:09 +0530

    4 files modified, 1 added registration feature in it

---
+++ src/app/data-models/user.model.ts
@@ -0,0 +1,6 @@
+export class UserDataModel{
+  name: string;
+  email: string;
+  password: string;
+  mobile_no: number;
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  email: string='';
+  constructor()  {
+    
+   }
   ngOnInit() {
+  }
+  doregesterMe(){
+    console.log("Registration was successful!");
+  }
+  sendOTP(){
+    alert("Enter Valid mobile no!");
   }
 
 }
